b0770020ba2df03bad21c64edc08fe1591c125f1
sar-bc <sar-bc@mail.ru>
2024-02-05 22:58:16 +0400

    add

--- main.py
+++ main.py
@@ -1559,27 +1559,40 @@
 
 # print("Данные для добавления на GitHub")
 
+# print("Изменения внесенные на другой машине")
 
 # s = ['ab_1', 'ac_2', 'bc_1', 'bc_2']
 # # a = [x for x in s if 'a' not in x]
 # # a = ['A' + x[1:] if x[0] == 'a' else 'B' + x[1:] for x in s]
 # a = ['A' + x[1:] if x[0] == 'a' else 'B' + x[1:] for x in s if x[1] == 'c']
 # print(a)
-# # тернальное выражение
-#
+# print(['A' + x[1:] if x[0] == 'a' else 'B' + x[1:] for x in ['ab_1', 'ac_2', 'bc_1', 'bc_2'] if x[1] == 'c'])
+# # тернарное выражение  q = True if условие else False
+# lst = []
+# for x in ['ab_1', 'ac_2', 'bc_1', 'bc_2']:
+#     if x[1] == 'c':
+#         if x[0] == 'a':
+#             lst.append('A' + x[1:])
+#         else:
+#             lst.append('B' + x[1:])
+# print(lst)
 
 # a = {0, 1, 2, 3}
 # b = {4, 3, 2, 1}
-# # c = a.union(b)  # {0, 1, 2, 3, 4}
+# c = a | b  # {0, 1, 2, 3, 4}
 # # c = a & b  # {1, 2, 3}
 # # c = a - b  # {0}
-# c = a ^ b  # {0, 4}
+# # c = a ^ b  # {0, 4}
+# # c = a.union(b)
 # print(c)
 # # a |= b
 # # a &= b
 # # a -= b
 # a ^= b
 # print(a)
+
+# c = a + b
+# a += b  => a = a + b
 
 # s1 = {1, 2}
 # s2 = {3}
@@ -1588,7 +1601,6 @@
 # s5 = {6}
 # s6 = {7, 8}
 # s7 = {9, 8}
-#
 # # s = s1.union(s2, s3, s4, s5, s6, s7)
 # s = s1 | s2 | s3 | s4 | s5 | s6 | s7
 # print(s)
@@ -1599,35 +1611,38 @@
 # s1 = "Hello"
 # s2 = "How are you"
 # s = set(s1) & set(s2)
+# print(s)
 # for i in s:
 #     print(i, end=" ")
-# # print()
 
 # s1 = "Python"
 # s2 = "Programming language"
 # s = set(s1) - set(s2)
 # print(s)
+# for i in s:
+#     print(i, end=" ")
 
-
-# drowing = {'Марина', 'Женя', 'Света'}
+# drawing = {'Марина', 'Женя', 'Света'}
 # music = {'Костя', 'Женя', 'Илья'}
-# one_hobby = drowing ^ music
+# one_hobby = drawing ^ music
 # print(one_hobby)
-# both_hobby = drowing & music
+# both_hobby = drawing & music
 # print(both_hobby)
-# drowing = drowing - both_hobby
-# print(drowing)
+#
+# drawing = drawing - both_hobby
+# print(drawing)
+
 
 # a = {0, 1, 2, 3, 4}
 # b = {3, 2, 1}
 # print(a < b)
 
-#  frozenset
+# frozenset
 # s = frozenset([1, 2, 3, 4, 5, 6])
 # s = frozenset("Hello")
 # print(s)
-#
-#  Словари (dict)
+
+# Словари (dict)
 
 # s = [1, 2, 3]
 # d = {"one": 1, "two": 2, "three": 3}
@@ -1639,49 +1654,51 @@
 # print(s1[1])
 # print(d1[2])
 
+
 # set() == {}
-# d = {0: "test", "one": 45, (1, 2.3): "Кортеж", True: 1, 35: [2, 3, 6, 7]}
+# d = {False: "test", 1: 45, (1, 2.3): "Кортеж", True: 1, 35: [2, 3, 6, 7], 0: "один", 54: {1: 2}}
 # print(d)
 # d[(1, 2.3)] = 100
 # print(d)
-#
+
 # d = {'one': 1, 'two': 2}
 # print(d)
 # print(type(d))
+
 #
-# d1 = dict(one=1, two=2)
+# d1 = dict(one='one', two=2)
 # print(d1)
 # print(type(d1))
 
-
+# d1 = dict(["one", 1, "two", 2])
 # d1 = dict([("one", 1), ("two", 2)])
+# d1 = dict(["on", ("two", 2)])
 # print(d1)
 
-# d = {x: x for x in range(7)}
+# d = {x: x ** 2 for x in range(7)}
 # print(d)
+
 
 # d = {"one": 1, "two": 2, "three": 3}
-# print("four" in d)
-# print(len(d))
-# for i in d:
-#     print(i, "->", d[i])
 # print(d)
-# key = "four"
+# # print("four" in d)
+# # print(len(d))
+# # for key in d:
+# #     print(key, "->", d[key])
+# key = "four"  # "four"
 # del d[key]
+# # if key in d:
+# #     print(d[key])
+# # try:
+# #     print(d[key])
+# # except KeyError:
+# #     print("Такого ключа не существует")
 # print(d)
-# if key in d:
-#     print(d[key])
-# print(d["four"])  # KeyError: 'four'
-# try:
-#     print(d[key])
-# except KeyError:
-#     print("Такого ключа нет")
 
-
-# s = {'x1': 3, 'x2': 7, 'x3': 5, 'x4': -1}
+# d = {'x1': 3, 'x2': 7, 'x3': 5, 'x4': -1}
 # res = 1
-# for i in s:
-#     res *= s[i]
+# for x in d:
+#     res *= d[x]
 # print(res)
 
 # d = dict()
@@ -1695,5 +1712,186 @@
 #     dislike = int(input("Какой элемент исключить: "))
 #     del d[dislike]
 # except (KeyError, ValueError):
-#     print("Такого элемента не существует")
+#     print("Такого элемента не существует не существует")
 # print(d)
+
+
+# mathematics = {"Матвей", "Евгения", "Михаил", "Максим", "Наталья"}
+# physics = {"Максим", "Матвей", "Александр"}
+#
+# all_st = mathematics | physics
+# print(all_st)
+#
+# both = mathematics & physics
+# print(both)
+#
+# # mathematics = both
+# # print(mathematics)
+#
+# print(mathematics - (physics ^ mathematics))  # {"Матвей",  "Максим"}
+
+# d = {'x1': 3, 'x2': 7, 'x3': [5, 6], 'x4': -1}
+# print(len(d))
+
+# goods = {
+#     "1": ["Core-i3-4330", 9, 4500],
+#     "2": ["Core i5-4670K", 3, 8500],
+#     "3": ["AMD FX-6300", 6, 3700],
+#     "4": ["Pentium G3220", 8, 2100],
+#     "5": ["Core i5-3450", 5, 6400],
+# }
+#
+# for key in goods:
+#     print(key, ") ", goods[key][0], " - ", goods[key][1], " шт. по ", goods[key][2], " руб.", sep="")
+#
+# while True:
+#     n = input("№: ")
+#     if n != "0":
+#         if n in goods:
+#             while True:
+#                 try:
+#                     count = int(input("Кол-во: "))
+#                     goods[n][1] += count
+#                     break
+#                 except ValueError:
+#                     print("Значение некорректное. Введите число")
+#         else:
+#             print("Такого ключа не существует")
+#     else:
+#         break
+#
+#
+# for key in goods:
+#     print(key, ") ", goods[key][0], " - ", goods[key][1], " шт. по ", goods[key][2], " руб.", sep="")
+
+# d = {'x1': 3, 'x2': 7, 'x3': 5}
+# print(d)
+# del d['x1']
+# d['x4'] = 10
+# print(d)
+# print(d.values())
+# print(d.keys())
+# print(d.items())
+# # for key, value in d.items():
+# #     print(key, "->", value)
+# print(list(d))  # ['x1', 'x2', 'x3']
+# print(list(d.values()))  # [3, 7, 5]
+# print(list(d.items()))  # [('x1', 3), ('x2', 7), ('x3', 5)]
+
+
+# d = {'x1': 3, 'x2': 7, 'x3': 5}
+#
+# d2 = d.copy()
+# print("d =", d)
+# print("d2 =", d2)
+#
+# d2["x4"] = 10
+# d['x1'] = 100
+#
+# print("d =", d)
+# print("d2 =", d2)
+
+# d = {'x1': 3, 'x2': 7, 'x3': 5}
+# print(d)
+# print(d["x1"])
+# value = d.get("x4", "Такого ключа не существует")
+# print(value)
+# item = d.pop("x1", 0)
+# print(item)
+# print(d)
+# item2 = d.popitem()
+# print(item2)
+# print(d)
+#
+# d.clear()
+# print(d)
+
+
+# d = {'x1': 3, 'x2': 7, 'x3': 5}
+# print(d)
+# # item = d.setdefault("x1", 10)
+# # print(item)
+# # print(d)
+# # a = {"one": 1, "two": 2, 'x1': 10}
+# # print(a)
+# # a = list(a.items())
+# a = [('one', 1), ('two', 2), ('x1', 10)]
+# print(a)
+# d.update(a)
+# print(d)
+
+
+# x = {'a': 1, 'b': 2}
+# y = {'b': 3, 'c': 4}
+# z = x | y
+# # z = x.copy()
+# # z.update(y)
+# print(z)
+
+# d = dict.fromkeys(['a', 'b', 'c'], 100)
+# print(d)
+
+# d = {'name': 'Kelly', 'age': 25, 'salary': 8000, 'city': 'New York'}
+# d2 = dict()
+# d2['name'] = d.pop("name")
+# d2['salary'] = d.pop("salary")
+# print(d)
+# print(d2)
+
+# d = {'name': 'Kelly', 'age': 25, 'salary': 8000, 'city': 'New York'}
+# d['location'] = d.pop('city')
+# print(d)
+
+# d = {
+#     'first': {
+#         1: {
+#             11: "abc",
+#             12: "abc",
+#             113: "abc",
+#         },
+#         2: {
+#             11: "abc"
+#         },
+#         3: {
+#             11: "abc"
+#         }
+#     },
+#     'second': {
+#         4: {
+#             11: "abc"
+#         },
+#         5:  {
+#             11: "abc"
+#         }
+#     }
+# }
+# print(d)
+#
+# for x in d:
+#     print(x)
+#     for y in d[x]:
+#         print("\t", y)
+#         for z in d[x][y]:
+#             print("\t\t", z, ":", d[x][y][z])
+
+# d = {'три': 3, 'один': 1, 'два': 2, 'четыре': 4}
+# # d2 = {value: key for key, value in d.items()}
+# d2 = {key: value for key, value in d.items() if value <= 2}
+# print(d2)
+# d2[1], d2[4] = d2[4], d2[1]
+# print(d2)
+
+
+a = ['one', 1, [2, 3], 'two', 10, 20, 'three', 15, 36, 60, 'four', -20]
+
+d = dict()
+s = None
+print(type(s))
+for i in a:
+    if type(i) == str:  # {'one': [1,2,3], 'two': [10,20], 'three': [], 'four': []}
+        d[i] = []  # d['two'] = []
+        s = i  # s = 'two'
+    else:
+        d[s].append(i)  # d['two'].append(20)
+
+print(d)
