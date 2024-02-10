fb9bc916a34d5349904dc29c5a05703b6cbc2fdb
sar-bc <sar-bc@mail.ru>
2024-02-10 15:37:55 +0400

    add

--- main.py
+++ main.py
@@ -1899,3 +1899,198 @@
 
 ########################################
 
+# zip()
+
+# one = [1, 2, 3, 4, 5]
+# two = ["one", "two", "three"]
+#
+# # three = [2.5, 4.6, 8.9]
+# d = dict(zip(two, one))
+# print(d)
+#
+# lst = list(zip(one, two, three))
+# print(lst)
+
+# f = {k: v for k, v in zip(one, two)}
+# print(f)
+
+# one = {"name": "Igor", "surname": "Doe", "job": "Consultant"}
+# two = {"name": "Irina", "surname": "Smith", "job": "Manager"}
+# three = {"name": "Irina", "surname": "Smith", "job": "Manager"}
+#
+# for (k1, v1), (k2, v2), (k3,v3) in zip(one.items(), two.items(), three.items()):
+#     print(k1, "->", v1)
+#     print(k2, "->", v2)
+#     print(k3, "->", v3)
+
+
+# lst = [(1, 'one'), (2, 'two'), (3, 'three')]
+# a, b = zip(*lst)
+# print(a)  # (1, 2, 3)
+# print(b)  # ('one', 'two', 'three')
+
+# a = {"one": 1, "two": 2, "three": 5}
+# b = {"three": 3, "four": 4}
+# print({**a, **b})  # {'one': 1, 'two': 2, 'three': 3, 'four': 4}
+#
+# for k, v in {**a, **b}.items():
+#     print(k, "->", v)
+
+# data = [5, 7, 9, 4, 1, 3, 5, 8, 6, 4]
+# data = ["red", "green", "blue"]
+# for num, color in enumerate(data, 1):
+#     print(num, ") ", color, sep="")
+# j = 1
+# for i in data:
+#     print(j, ") ", i, sep="")
+#     j += 1
+
+# a = [1, 2, 3]
+# b = [*a, 4, 5, 6]  # [1, 2, 3, 4, 5, 6]
+# print(b)
+
+# def func(*args):
+#     return args
+#
+#
+# print(func(5))
+# print(func(5, 6, 7, "abc"))
+# print(func())
+
+# def summa(*args):
+#     res = 0
+#     for i in args:
+#         res += i
+#     return res
+#
+#
+# print(summa(1, 3, 6, 5, 4, 5, 9, 8, 7, 6))
+# print(summa(4, 2, 6))
+
+
+# def to_dict(*args):
+#     # return dict(zip(args, args))
+#     # d = {}
+#     # for i in args:
+#     #     d.update({i: i})
+#     # return d
+#     return {i: i for i in args}
+#
+#
+# print(to_dict(1, 2, 3, 4))
+# print(to_dict("grey", (2, 17), 3.11, -4))
+
+
+# def ch(*args):
+#     average = sum(args) / len(args)
+#     print(average)
+#     res = []
+#     for num in args:
+#         if average > num:
+#             res.append(num)
+#     return res
+#
+#
+# print(ch(1, 2, 3, 4, 5, 6, 7, 8, 9))
+# print(ch(3, 6, 1, 9, 5))
+#
+
+#
+# def func(a, *args):
+#     return a, args
+#
+#
+# print(func(5, 9, 8, 7, 6))
+
+#
+# def print_scores(student, *scores):
+#     print("Name: ", student)
+#     for score in scores:
+#         print(score, end=" ")
+#     print()
+#
+#
+# print_scores("Roman", 5, 4, 3, 5, 4, 5, 5, 3, 5)
+# print_scores("Nikita", 5, 3, 5, 3)
+
+
+# def func(**kwargs):
+#     return kwargs
+#
+#
+# print(func(a=1, b=2, c=3))
+# print(func())
+# print(func(name="Python"))
+
+
+# def intro(**kwargs):
+#     for k, v in kwargs.items():
+#         print(k, "is", v)
+#     print()
+#
+#
+# intro(name="Irina", surname="Sharma", age=22)
+# intro(name="Igor", surname="Wood", email="igor@gmail.com", age=22, phone=987654321)
+
+
+# def func(a, b, *args, dd=5, cc=7, **kwargs):
+#     return a, b, args, kwargs, dd, cc
+#
+#
+# print(func(1, 2, 3, 4, 5, cc=3, aa=1, bb=2))
+
+# def db(**kwargs):
+#     my_dict.update(**kwargs)
+#
+#
+# my_dict = {"one": "first"}
+#
+# db(k1=22, k2=31, k3=11, k4=91)
+# # db(name="Bob", age=31, weight=61, eyes_color="grey")
+# print(my_dict)
+
+
+# name = "Tom"
+# surname = ""
+#
+#
+# def hi():
+#     global name, surname
+#     name = "Sam"
+#     surname = "Johnson"
+#     print("Hello", name, surname)
+#
+#
+# def bye():
+#     print("Good bye,", name)
+#
+#
+# print(name)
+# hi()
+# bye()
+# print(name)
+# print(surname)
+
+#
+# i = 5
+#
+#
+# def func(arg=i):
+#     print(arg)
+#
+#
+# i = 6
+# func()
+
+
+# def func(a):
+#     x = 2
+#
+#     def inner():
+#         print("x:", x)
+#         return a + x
+#     return inner()
+#
+#
+# print(func(3))
+
