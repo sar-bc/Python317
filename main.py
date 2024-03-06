5b634a64c37868c3ee9418643ed24c23c87c1a98
sar-bc <sar-bc@mail.ru>
2024-03-06 22:25:56 +0400

    add

--- main.py
+++ main.py
@@ -3407,6 +3407,104 @@ import re
 # print(f.closed)
 
 
-with open("test2.txt", 'r') as f:
-    for line in f:
-        print(line[:3])
+# with open("test2.txt", 'r') as f:
+#     for line in f:
+#         print(line[:3])
+# def get_line(lt):
+#     lt = map(str, lt)
+#     return ' '.join(lt)
+#
+#
+# file_name = "res.txt"
+# lst = [4.5, 2.8, 3.9, 1.0, 0.3, 4.33, 7.777]
+# with open(file_name, 'w') as f:
+#     f.write(get_line(lst))
+#
+# with open(file_name, 'r') as f:
+#     st = f.read()
+#
+# print(st)
+# print(type(st))
+#
+# nums = list(map(float, st.split()))
+# print(nums)
+
+# def longest_worlds(file):
+#     with open(file, 'r', encoding='utf-8') as text:
+#         w = text.read().split()
+#         max_length = len(max(w, key=len))
+#         res = [i for i in w if len(i) == max_length]
+#         if len(res) == 1:
+#             return res[0]
+#         return res
+#
+#
+# print(longest_worlds('test.txt'))
+
+
+# text = "Строка №1\nСтрока №2\nСтрока №3\nСтрока №4\nСтрока №5\nСтрока №6\nСтрока №7\nСтрока №8\nСтрока №9\nСтрока №10\n"
+#
+# # with open('one.txt', 'w') as f:
+# #     f.write(text)
+#
+# with open('one.txt', 'r') as fr, open('two.txt', 'w') as fw:
+#     for line in fr:
+#         lene = line.replace("Строка", "Линия -")
+#         fw.write(line)
+#
+
+import os
+
+# import os.path
+
+
+# print(os.getcwd())
+# print(os.listdir())
+
+# os.mkdir("folder1")
+# os.makedirs("nested1/nested2/nested3")
+
+# os.rmdir("folder1")
+# os.remove("test.txt")
+# os.rename()
+
+# for root, dirs, files in os.walk("nested1", topdown=False):
+#     print("Root:", root)
+#     print("\tSubdirs:", dirs)
+#     print("\t\tFiles:", files)
+
+# def remove_empty_dirs(root_tree):
+#     print(f"Удаление пустых директорий и папок {root_tree}")
+#     print("-" * 50)
+#     for root, dirs, files in os.walk(root_tree):
+#         if not os.listdir(root):
+#             os.rmdir(root)
+#             print(f"Директория {root} удалена")
+#     print("-" * 50)
+#
+#
+# remove_empty_dirs("nested1")
+
+# print(os.path.split(r"D:\STUDIES\Python317\nested1"))  # [1]
+# print(os.path.join(r'D'))
+
+# dirs = [r'Work\F1', r'Work\F2\F21']
+# for d in dirs:
+#     os.makedirs(d)
+
+# files = {
+#     'Work': ['w.txt'],
+#     r'Work\F1': ['f11.txt', 'f12.txt', 'f13.txt'],
+#     r'Work\F2\F21': ['f211.txt', 'f212.txt']
+# }
+#
+# for dir1, files in files.items():
+#     for file in files:
+#         file_path = os.path.join(dir1, file)
+#         open(file_path, 'w').close()
+#
+# file_with_text = [r'Work\m.txt', r'Work\F1\f12.txt', r'Work\F1\f12.txt']
+# for file in file_with_text:
+#     with open(file, 'w') as f:
+#         f.write(f"Текст в файле {file}")
+#
